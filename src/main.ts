d627e8a235463d57b4290deb5b183a28e7e8c70c
kodynit <kodynit777@gmail.com>
2025-11-23 19:11:48 +0000

    Edited main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,              // يمنع أي بيانات زايدة
+      forbidNonWhitelisted: true,   // يعطي خطأ لو أرسلوا Keys ممنوعة
+      transform: true,              // يحول الأنواع تلقائيًا (string → number)
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
