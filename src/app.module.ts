4b47a0f7a1fc6d45ee0f57ecef277ff11d504e84
kodynit777-dev <kodynit777@gmail.com>
2025-12-23 14:38:35 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { PrismaModule } from '../prisma/prisma.module';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
@@ -13,12 +13,9 @@ import { OrdersModule } from './orders/orders.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath:
-        process.env.NODE_ENV === 'production'
-          ? '.env.production'
-          : '.env.development',
     }),
-    PrismaModule,
+
+    // ✅ Project modules
     AuthModule,
     UsersModule,
     RestaurantsModule,
