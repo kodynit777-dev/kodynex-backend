376b87faf2330abc1b7e3a1f244fdc44a4ef7bd6
kodynit777-dev <kodynit777@gmail.com>
2026-01-20 18:55:49 +0300

    Update public.controller.ts

--- src/public/public.controller.ts
+++ src/public/public.controller.ts
@@ -1,1 +1,15 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { PublicService } from './public.service';
 
+@Controller('public')
+export class PublicController {
+  constructor(private readonly publicService: PublicService) {}
+
+  /**
+   * GET /api/public/:tenant/catalog
+   */
+  @Get(':tenant/catalog')
+  async getCatalog(@Param('tenant') tenant: string) {
+    return this.publicService.getCatalogByTenant(tenant);
+  }
+}
