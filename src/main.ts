37d73b2bcf9fbbfdd4aa947fd52a588575c67f52
Bandar <kodynit777@gmail.com>
2026-02-23 19:32:19 +0300

    chore: bump version to v43

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
- console.log('🚀 VERSION: v52');
+console.log('🚀 VERSION: v43');
 
   const app = await NestFactory.create(AppModule);
 
