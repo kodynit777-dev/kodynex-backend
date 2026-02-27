85cfff4615cbed3adb67cc15b548279a98cf34da
bandarbnalii <bandarbnali@gmail.com>
2026-02-27 22:05:14 +0300

    main2222

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  console.log('🚀 VERSION: v48');
+  console.log('🚀 VERSION: v49');
 
   const app = await NestFactory.create(AppModule);
 
